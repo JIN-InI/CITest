8ab77d2967eaafd8ce231ddc55885c085999b521
InI <jin@rep88.com>
2021-10-26 19:49:04 +0900

    first

---
+++ src/main/java/tokyo/ini/buildtest/BuildTest.java
@@ -0,0 +1,17 @@
+package tokyo.ini.buildtest;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class BuildTest extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
